32a35d0ecb9527661fc1cb6ae2074bda74927be1
Krunal-97 <42610373+Krunal-97@users.noreply.github.com>
2019-03-12 17:54:50 +0530

    Initial commit

---
+++ friend/includefrd/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "quiz";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
---
+++ user/includeuser/showme.php
@@ -0,0 +1,17 @@
+
+<?php
+	include 'header3.php';
+?>
+
+<section>
+		<div class="link">
+			<span><b>Friend Names : Score</b><br></span>
+			<?php
+			include 'getscores.php';
+			?>
+		</div>
+</section>
+
+<?php 
+	include 'footer3.php';
+?>
